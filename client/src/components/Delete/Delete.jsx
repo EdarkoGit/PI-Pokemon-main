7a6a1a944a79233e7a6d8e4189ddcc66288e0d37
Edwin Ortiz <edarcode@gmail.com>
2021-12-18 07:52:04 -0500

    front: creando componente delete

---
+++ client/src/components/Delete/Delete.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { DeleteStyle } from "./style";
+
+const Delete = () => {
+  return <DeleteStyle>delete</DeleteStyle>;
+};
+
+export default Delete;
---
+++ client/src/components/Delete/style.js
@@ -0,0 +1,4 @@
+import styled from "styled-components";
+export const DeleteStyle = styled.section`
+  background-color: lightgrey;
+`;
