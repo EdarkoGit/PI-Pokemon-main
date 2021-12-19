0f334e24984a28acec73a4fd590a8047e8348ca2
Edwin Ortiz <edarcode@gmail.com>
2021-12-19 02:24:41 -0500

    front: seteando state allPokemons

--- client/src/redux/actions/pokemons.js
+++ client/src/redux/actions/pokemons.js
@@ -1,9 +1,32 @@
 import { actionGenerator, axiosGet } from "../../utils/actions";
-import { SET_POKEMON, URL_BASE_BACKEND } from "../constants/pokemons";
+import { SET_WHAT_RENDER } from "../constants/flags";
+import {
+  SET_ALL_POKEMONS,
+  SET_POKEMON,
+  URL_BASE_BACKEND,
+} from "../constants/pokemons";
 
 export const getPokemon = (name) => {
   return async (dispatch) => {
-    const payload = await axiosGet(`${URL_BASE_BACKEND}/pokemons?name=${name}`);
-    dispatch(actionGenerator(SET_POKEMON, payload));
+    try {
+      const payload = await axiosGet(
+        `${URL_BASE_BACKEND}/pokemons?name=${name}`
+      );
+      dispatch(actionGenerator(SET_POKEMON, payload));
+      dispatch(actionGenerator(SET_WHAT_RENDER, "pokemon"));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+export const getAllPokemons = () => {
+  return async (dispatch) => {
+    try {
+      const payload = await axiosGet(`${URL_BASE_BACKEND}/pokemons`);
+      dispatch(actionGenerator(SET_ALL_POKEMONS, payload));
+      dispatch(actionGenerator(SET_WHAT_RENDER, "allPokemon"));
+    } catch (error) {
+      console.log(error);
+    }
   };
 };
--- client/src/redux/constants/pokemons.js
+++ client/src/redux/constants/pokemons.js
@@ -1,2 +1,3 @@
 export const SET_POKEMON = "SET_POKEMON";
+export const SET_ALL_POKEMONS = "SET_ALL_POKEMONS";
 export const URL_BASE_BACKEND = "http://localhost:3001";
