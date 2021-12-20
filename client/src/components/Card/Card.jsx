99301bb0f5853460923f26741d64efef21b274f0
Edwin Ortiz <edarcode@gmail.com>
2021-12-20 17:09:04 -0500

    front: paginado funcionando, faltan detalles

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { CardStyle } from "./style";
-import { Link } from "react-router-dom";
 import { renderTypes } from "../../utils/maps";
 
 const Card = ({ id, name, img, types }) => {
   return (
-    <CardStyle>
+    <CardStyle to={`detail/${id}`}>
       {id ? (
-        <Link to={`detail/${id}`}>
+        <div>
           <picture>
             <img src={img} alt={name} />
           </picture>
           <h2>{name}</h2>
           {renderTypes(types)}
-        </Link>
+        </div>
       ) : null}
     </CardStyle>
   );
--- client/src/redux/constants/pokemons.js
+++ client/src/redux/constants/pokemons.js
@@ -1,3 +1,4 @@
 export const SET_POKEMON = "SET_POKEMON";
 export const SET_ALL_POKEMONS = "SET_ALL_POKEMONS";
+export const SET_SLICE_POKEMONS = "SET_SLICE_POKEMONS";
 export const URL_BASE_BACKEND = "http://localhost:3001";
